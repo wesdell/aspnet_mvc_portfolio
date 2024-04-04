636c960353019c8e283bbbced707ae99ab929613
Wesdell <wesdell17@gmail.com>
2024-04-04 14:38:39 -0500

    Extract and create new method to get projects data.

---
+++ portfolio/Services/ProjectsMock.cs
@@ -0,0 +1,42 @@
+﻿using portfolio.Models;
+
+namespace portfolio.Services
+{
+	public class ProjectsMock
+	{
+		public List<ProjectViewModel> GetProjects()
+		{
+			return new List<ProjectViewModel>()
+			{
+				new ProjectViewModel()
+				{
+					Title = "Lorem",
+					Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+					ImageURL = "/img/unknown.jpg",
+					Link = "https://amazon.com"
+				},
+				new ProjectViewModel()
+				{
+					Title = "Ipsum",
+					Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+					ImageURL = "/img/unknown.jpg",
+					Link = "https://facebook.com"
+				},
+				new ProjectViewModel()
+				{
+					Title = "Lorem",
+					Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+					ImageURL = "/img/unknown.jpg",
+					Link = "https://amazon.com"
+				},
+				new ProjectViewModel()
+				{
+					Title = "Ipsum",
+					Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+					ImageURL = "/img/unknown.jpg",
+					Link = "https://facebook.com"
+				}
+			};
+		}
+	}
+}
