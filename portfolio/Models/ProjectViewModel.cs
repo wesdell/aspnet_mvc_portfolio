43cf7ccee86d405d6daaebbc702db09cb470e7c0
Wesdell <wesdell17@gmail.com>
2024-04-03 22:14:42 -0500

    New model to pass projects data.

---
+++ portfolio/Models/ProjectViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace portfolio.Models
+{
+	public class ProjectViewModel
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string ImageURL { get; set; }
+		public string Link { get; set; }
+
+	}
+}
