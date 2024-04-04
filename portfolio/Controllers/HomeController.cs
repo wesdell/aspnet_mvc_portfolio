41840212391fd6d2bb1f9f82709a179790014c8a
Wesdell <wesdell17@gmail.com>
2024-04-03 22:16:11 -0500

    New method to get projects data.

--- portfolio/Controllers/HomeController.cs
+++ portfolio/Controllers/HomeController.cs
@@ -4,35 +4,67 @@ using System.Diagnostics;
 
 namespace portfolio.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            User user = new User()
-            {
-                Name = "John Doe",
-                Age = 20
-            };
+		private List<ProjectViewModel> GetProjects()
+		{
+			return new List<ProjectViewModel>()
+			{
+				new ProjectViewModel()
+				{
+					Title = "Lorem",
+					Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+					ImageURL = "/img/unknown.jpg",
+					Link = "https://amazon.com"
+				},
+				new ProjectViewModel()
+				{
+					Title = "Ipsum",
+					Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+					ImageURL = "/img/unknown.jpg",
+					Link = "https://facebook.com"
+				},
+				new ProjectViewModel()
+				{
+					Title = "Lorem",
+					Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+					ImageURL = "/img/unknown.jpg",
+					Link = "https://amazon.com"
+				},
+				new ProjectViewModel()
+				{
+					Title = "Ipsum",
+					Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+					ImageURL = "/img/unknown.jpg",
+					Link = "https://facebook.com"
+				}
+			};
+		}
 
-            return View(user);
-        }
+		public IActionResult Index()
+		{
+			List<ProjectViewModel> projects = GetProjects().Take(3).ToList();
+			HomeIndexViewModel model = new HomeIndexViewModel() { Projects = projects };
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			return View(model);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
