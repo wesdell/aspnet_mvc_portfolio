a91b9f36e57b9a960ae3c94823da92ff4f4a283d
Wesdell <wesdell17@gmail.com>
2024-04-04 15:32:12 -0500

    Create User and Projects interface.

---
+++ portfolio/Interfaces/Projects.cs
@@ -0,0 +1,16 @@
+﻿using portfolio.Models;
+
+namespace portfolio.Interfaces
+{
+	public interface IProject
+	{
+		string Title { get; set; }
+		string Description { get; set; }
+		string ImageURL { get; set; }
+		string Link { get; set; }
+	}
+	public interface IProjects
+	{
+		List<ProjectViewModel> GetProjects();
+	}
+}
---
+++ portfolio/Interfaces/Users.cs
@@ -0,0 +1,15 @@
+﻿using portfolio.Models;
+
+namespace portfolio.Interfaces
+{
+	public interface IUser
+	{
+		string Name { get; set; }
+		int Age { get; set; }
+	}
+
+	public interface IUsers
+	{
+		User GetUser();
+	}
+}
