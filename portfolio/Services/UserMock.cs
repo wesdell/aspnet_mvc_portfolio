733654978c4579649ba12b8099b311a41c9eca44
Wesdell <wesdell17@gmail.com>
2024-04-04 14:38:51 -0500

    Extract and create new method to get users data.

---
+++ portfolio/Services/UserMock.cs
@@ -0,0 +1,16 @@
+﻿using portfolio.Models;
+
+namespace portfolio.Services
+{
+	public class UserMock
+	{
+		public User GetUser()
+		{
+			return new User()
+			{
+				Name = "John Doe",
+				Age = 20
+			};
+		}
+	}
+}
