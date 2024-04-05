f729b2e4052dea3ab4784593b18466362e201ea4
Wesdell <wesdell17@gmail.com>
2024-04-05 15:23:04 -0500

    New model to get form data.

---
+++ portfolio/Models/ContactViewModel.cs
@@ -0,0 +1,11 @@
+﻿using portfolio.Interfaces;
+
+namespace portfolio.Models
+{
+	public class ContactViewModel : IContact
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Message { get; set; }
+	}
+}
