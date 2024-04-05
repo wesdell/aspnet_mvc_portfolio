3f2df299fc08ccca9e2c547fe037c1668b7db2ff
Wesdell <wesdell17@gmail.com>
2024-04-05 15:08:09 -0500

    New method to fire Contact view.

--- portfolio/Controllers/HomeController.cs
+++ portfolio/Controllers/HomeController.cs
@@ -5,35 +5,40 @@ using System.Diagnostics;
 
 namespace portfolio.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IUsers _user;
-        private readonly IProjects _projects;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IUsers _user;
+		private readonly IProjects _projects;
 
-        public HomeController(ILogger<HomeController> logger, IUsers user, IProjects projects)
-        {
-            this._logger = logger;
-            this._user = user;
-            this._projects = projects;
-        }
+		public HomeController(ILogger<HomeController> logger, IUsers user, IProjects projects)
+		{
+			this._logger = logger;
+			this._user = user;
+			this._projects = projects;
+		}
 
-        public IActionResult Index()
-        {
-            HomeIndexViewModel model = new HomeIndexViewModel() { Projects = _projects.GetProjects().Take(3).ToList(), User = _user.GetUser() };
-            return View(model);
-        }
+		public IActionResult Index()
+		{
+			HomeIndexViewModel model = new HomeIndexViewModel() { Projects = _projects.GetProjects().Take(3).ToList(), User = _user.GetUser() };
+			return View(model);
+		}
 
-        public IActionResult Projects()
-        {
-            List<ProjectViewModel> model = _projects.GetProjects();
-            return View(model);
-        }
+		public IActionResult Projects()
+		{
+			List<ProjectViewModel> model = _projects.GetProjects();
+			return View(model);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
